9223a7681f2b6af6fb202b11be69bfe0dd375de5
Uzair Rahim <uzair.rahim@redbookconnect.com>
2014-08-06 20:19:38 -0500

    Adding comments

--- configs/app-config.js
+++ configs/app-config.js
@@ -68,8 +68,10 @@ require(["app", "approuter"],function(App, ApplicationRouter){
 	
 	App.router = router;
 		router.on("route", function(){
-			if(App.session.isLoggedIn()){
+			// if user is logged in and verified switch the layout to app mode
+			if(App.session.isLoggedIn() && App.session.isVerified()){
 				App.layout.toggleLayout("app");
+			// if user is not logged in or verified switch the layout to portal mode
 			}else{
 				App.layout.toggleLayout("portal");
 			}
--- scripts/controllers/app-controller.js
+++ scripts/controllers/app-controller.js
@@ -20,35 +20,42 @@ define([
 
 			main : function(){
 				console.log("Main route...");
+				// If user is not logged in go to login screen
 				if(!App.session.isLoggedIn()){
 					App.router.navigate("login", true);
-				}else{
+				// If user is logged in and verified go to jobs screen	
+				}else if(App.session.isLoggedIn() && App.session.isVerified()){
 					App.router.navigate("jobs", true);
 				}
 			},
 
 			login : function(){
 				console.log("Login route...");
-				if(App.session.isLoggedIn()){
-					App.router.navigate("jobs", true);
-				}else{
+				// If user is not logged in go to login screen
+				if(!App.session.isLoggedIn()){
 					var view = new Login();
 					App.layout.body.show(view);
+				// If user is logged in and verified go to jobs screen	
+				}else if(App.session.isLoggedIn() && App.session.isVerified()){
+					App.router.navigate("jobs", true);
 				}
 			},
 
 			jobs : function(){
 				console.log("Jobs route...");
-				if(!App.session.isLoggedIn()){
-					App.router.navigate("login", true);
-				}else{
+				// If user is logged in and verified go to jobs screen
+				if(App.session.isLoggedIn() && App.session.isVerified()){
 					var view = new Jobs();
 					App.layout.body.show(view);
+				// If user is not logged in or is not verified go to login screen	
+				}else{
+					App.router.navigate("login", true);
 				}
 			},
 
 			logout : function(){
 				console.log("Logout route...");
+				// Set logged to false and redirect to login screen
 				App.session.set({logged : false});
 				App.router.navigate("login", true);
 			},
@@ -58,6 +65,8 @@ define([
 			redirectOnLogin : function(){
 				if(App.session.isVerified()){
 					App.router.navigate("jobs", true);
+				}else{
+					alert("User is not verified");
 				}
 			}			
 		});
